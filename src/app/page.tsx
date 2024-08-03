e743f5d9c87fab7d0e5e94b15ed76da17f275d22
Dayana Mariano <dayanaraquelle@gmail.com>
2024-08-03 18:29:28 -0300

    :sparkles: feat: Add sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 import Image from "next/image";
-import { IoMdClose } from "react-icons/io";
+import Link from 'next/link'
+import CloseIcon from '@mui/icons-material/Close';
+import HouseIcon from '@mui/icons-material/House';
+import ForumIcon from '@mui/icons-material/Forum';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import InsertLinkIcon from '@mui/icons-material/InsertLink';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import ViewCarouselIcon from '@mui/icons-material/ViewCarousel';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export default function Home() {
   return (
@@ -16,7 +27,7 @@ export default function Home() {
                 className="w-[140px] h-[45px]"
               />
               <button className="p-1 hover:bg-[#bab8b825] hover:rounded-full">
-                <IoMdClose className="text-white size-7 w-[37px]"/>
+                <CloseIcon className="text-white size-7 w-[37px]"/>
               </button>
             </div>
             <span className="text-[#545454] font-bold text-sm mt-2">YOUR PACTTO PROFILE</span>
@@ -42,6 +53,49 @@ export default function Home() {
             <p className="text-[#ACACAC] text-sm">Pactto member since 2024</p>
           </div>
         </div>
+        <nav>
+          <Link href="/" id="/" className="flex flex-row text-[#BDBDBD] text-sm items-center w-full h-12 px-8 target:bg-[#3a363f] target:text-[#1DBBA5] hover:bg-[#4C4851]">
+            <HouseIcon className="text-[#BDBDBD] target:text-[#1DBBA5] size-6 mr-3"/>
+            Home
+          </Link>
+          <Link href="/" id="/" className="flex flex-row text-[#BDBDBD] text-sm items-center w-full h-12 px-8 target:bg-[#3a363f] target:text-[#1DBBA5] hover:bg-[#4C4851]">
+            <ForumIcon className="text-[#BDBDBD] target:text-[#1DBBA5] size-6 mr-3"/>
+            Pacttos (Chats)
+          </Link>
+          <Link href="/" id="/" className="flex flex-row text-[#BDBDBD] text-sm items-center w-full h-12 px-8 target:bg-[#3a363f] target:text-[#1DBBA5] hover:bg-[#4C4851]">
+            <VideoLibraryIcon className="text-[#BDBDBD] target:text-[#1DBBA5] size-6 mr-3"/>
+            Items to review
+          </Link>
+          <Link href="/" id="/" className="flex flex-row text-[#BDBDBD] text-sm items-center w-full h-12 px-8 target:bg-[#3a363f] target:text-[#1DBBA5] hover:bg-[#4C4851]">
+            <InsertLinkIcon className="text-[#BDBDBD] target:text-[#1DBBA5] size-6 mr-3"/>
+            Web links you created
+          </Link>
+          <Link href="/" id="/" className="flex flex-row text-[#BDBDBD] text-sm items-center w-full h-12 px-8 target:bg-[#3a363f] target:text-[#1DBBA5] hover:bg-[#4C4851]">
+            <ManageAccountsIcon className="text-[#BDBDBD] target:text-[#1DBBA5] size-6 mr-3"/>
+            Personal information
+          </Link>
+          <Link href="/" id="/" className="flex flex-row text-[#BDBDBD] text-sm items-center w-full h-12 px-8 target:bg-[#3a363f] target:text-[#1DBBA5] hover:bg-[#4C4851]">
+            <CardGiftcardIcon className="text-[#BDBDBD] target:text-[#1DBBA5] size-6 mr-3"/>
+            Revies packages for sale
+          </Link>
+          <Link href="/" id="/" className="flex flex-row text-[#BDBDBD] text-sm items-center w-full h-12 px-8 target:bg-[#3a363f] target:text-[#1DBBA5] hover:bg-[#4C4851]">
+            <ViewCarouselIcon className="text-[#BDBDBD] target:text-[#1DBBA5] size-6 mr-3"/>
+            Paccto websites
+          </Link>
+          <Link href="/" id="/" className="flex flex-row text-[#BDBDBD] text-sm items-center w-full h-12 px-8 target:bg-[#3a363f] target:text-[#1DBBA5] hover:bg-[#4C4851]">
+            <OndemandVideoIcon className="text-[#BDBDBD] target:text-[#1DBBA5] size-6 mr-3"/>
+            Reference video library
+          </Link>
+          <Link href="/" id="/" className="flex flex-row text-[#BDBDBD] text-sm items-center w-full h-12 px-8 target:bg-[#3a363f] target:text-[#1DBBA5] hover:bg-[#4C4851]">
+            <SubscriptionsIcon className="text-[#BDBDBD] target:text-[#1DBBA5] size-6 mr-3"/>
+            Subscription
+          </Link>
+          <Link href="/" id="/" className="flex flex-row text-[#BDBDBD] text-sm items-center w-full h-12 px-8 target:bg-[#3a363f] target:text-[#1DBBA5] hover:bg-[#4C4851]">
+            <SettingsIcon className="text-[#BDBDBD] target:text-[#1DBBA5] size-6 mr-3"/>
+            Review Settings
+          </Link>
+        </nav>
+        <button className="bg-[#B8B8B8] w-[193px] h-12 m-auto mb-4 rounded-3xl text-[#181818] text-sm font-semibold hover:bg-[#989898]">LOGOUT</button>
       </aside>
       <main className="flex min-h-screen flex-col items-center justify-between p-24 flex-initial">
         dashboard
